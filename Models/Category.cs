971b7dee14569e7057b04de93354fd0ea341a7d5
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-01-16 17:45:21 -0500

    Add data annotations to Category and Task models for validation and relationships

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,10 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace tasksef.Models
 {
   public class Category
   {
+    // Key es un atributo (data annotation) que indica que la propiedad Id es la clave primaria de la entidad
+    [Key]
     public Guid Id { get; set; }
+
+    [Required]
+    [MaxLength(80)]
     public string Name { get; set; }
+
     public string Description { get; set; }
+
     // de esta forma se establece la relación con la clase Task y si queremos incluir una colección de tareas en la clase Category en una consulta, se puede hacer
     public virtual ICollection<Task> Tasks { get; set; }
   }
--- Models/Task.cs
+++ Models/Task.cs
@@ -1,15 +1,33 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace tasksef.Models;
 
 public class Task
 {
+  [Key]
   public Guid Id { get; set; }
+
+  [ForeignKey("CategoryId")]
   public Guid CategoryId { get; set; }
+
+  [Required]
+  [MaxLength(80)]
   public string Title { get; set; }
+
   public string Description { get; set; }
+
   public bool IsComplete { get; set; }
+
   public Priority Priority { get; set; }
+
   public DateTime CreatedAt { get; set; }
+
   public virtual Category Category { get; set; }
+
+  // con esta anotación se indica que la propiedad Resume no se mapea a la base de datos
+  [NotMapped]
+  public string Resume { get; set; }
 }
 
 public enum Priority
