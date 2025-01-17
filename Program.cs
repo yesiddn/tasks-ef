4c31da05e89fc6a2440336ccc7b2363f408120c9
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-01-16 21:17:30 -0500

    Configure in-memory database for TasksContext and add endpoint to verify database creation

--- Program.cs
+++ Program.cs
@@ -1,6 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using tasksef;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// de esta forma se puede configurar el contexto de la base de datos en memoria
+builder.Services.AddDbContext<TasksContext>(opt => opt.UseInMemoryDatabase("TasksDB"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/dbConection", ([FromServices] TasksContext dbContext) =>
+  {
+    // se asegura que la base de datos se haya creado, si no existe la crea
+    dbContext.Database.EnsureCreated();
+
+    // retorna true si la base de datos esta creada en memoria
+    return Results.Ok($"Database created: {dbContext.Database.IsInMemory()}");
+  }
+);
 
 app.Run();
