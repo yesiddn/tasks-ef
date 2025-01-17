391d007e0e93d4f785d76a7e62da5a9536d9d47d
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-01-16 22:00:31 -0500

    Configure SQL Server database connection for TasksContext

--- Program.cs
+++ Program.cs
@@ -1,11 +1,17 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using tasksef;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // de esta forma se puede configurar el contexto de la base de datos en memoria
-builder.Services.AddDbContext<TasksContext>(opt => opt.UseInMemoryDatabase("TasksDB"));
+// builder.Services.AddDbContext<TasksContext>(opt => opt.UseInMemoryDatabase("TasksDB"));
+
+// al metodo AddSqlServer se le pasa el contexto y la cadena de conexion
+// Data Source=nombre del servidor;Initial Catalog=nombre de la base de datos;User Id=usuario;Password=contraseña;TrustServerCertificate=True;
+// TrustServerCertificate=True; es para que no se muestre el error de certificado no confiable
+builder.Services.AddSqlServer<TasksContext>("Data Source=YESID\\SQLEXPRESS;Initial Catalog=Tasks;user id=sa;password=root;TrustServerCertificate=True;");
 
 var app = builder.Build();
 
