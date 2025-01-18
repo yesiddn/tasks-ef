0a1142ad7b6c3cd723abbb5e134c63da186c1001
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-01-17 20:55:57 -0500

    Refactor Category model to use Fluent API for configuration and remove data annotations

--- Models/Category.cs
+++ Models/Category.cs
@@ -5,11 +5,11 @@ namespace tasksef.Models
   public class Category
   {
     // Key es un atributo (data annotation) que indica que la propiedad Id es la clave primaria de la entidad
-    [Key]
+    // [Key]
     public Guid Id { get; set; }
 
-    [Required]
-    [MaxLength(80)]
+    // [Required]
+    // [MaxLength(80)]
     public string Name { get; set; }
 
     public string Description { get; set; }
--- TasksContext.cs
+++ TasksContext.cs
@@ -12,4 +12,16 @@ public class TasksContext : DbContext {
 
   // DbContextOptions es una clase que contiene la configuración de la base de datos
   public TasksContext(DbContextOptions<TasksContext> options) : base(options) { }
+
+  // no es buena practica combinar data annotations con fluent api, aunque en dado caso de hacerlo, se le da prioridad a fluent api porque se ejecuta después de data annotations
+  protected override void OnModelCreating(ModelBuilder modelBuilder) {
+    modelBuilder.Entity<Category>(category =>
+    {
+      category.ToTable("Category");
+      category.HasKey(p => p.Id);
+
+      category.Property(p => p.Name).IsRequired().HasMaxLength(80);
+      category.Property(p => p.Description).HasMaxLength(150);
+    });
+  }
 }
